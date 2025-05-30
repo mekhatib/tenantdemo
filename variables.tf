f8d34edd8cfde05c39bd1de9d79e428bcaf16dfd
mekhatib <melkhatib@ziggo.nl>
2025-05-30 20:05:54 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,11 @@
 variable "environment" {
   description = "Environment name"
   type        = string
-  default     = "dev"
 }
 
 variable "project_name" {
   description = "Project name"
   type        = string
-  default     = "rfp-poc"
 }
 
 variable "aws_region" {
